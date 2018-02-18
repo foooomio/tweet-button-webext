29c608ddf513a53f914bfe637655bc3066695654
foooomio <foooomio@gmail.com>
2018-02-18 18:12:46 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Tweet Button Webext
+Web extention to add a tweet button to toolbar
+
+## License
+This software is released under the [MIT License](https://github.com/foooomio/tweet-button-webext/blob/master/LICENSE.txt).
+
+## Credits
+[Icons](https://www.iconfinder.com/icons/771365/circle_logo_media_network_share_social_twitter_icon) by [Martial Red](https://www.iconfinder.com/MaxIcon) is licensed under [CC BY 3.0](https://creativecommons.org/licenses/by/3.0/).
+
+## Author
+foooomio - [@foooomio](https://twitter.com/foooomio)
---
+++ src/background.js
@@ -0,0 +1,37 @@
+'use strict'
+
+// Array Util
+const first = array =>
+  Array.isArray(array) && array.length > 0 ? array[0] : null
+
+// Quotation
+const lqm = '“'
+const rqm = '”'
+const quote = text => lqm + text + rqm
+
+// URL Builder
+const buildUrlParams = params =>
+  Object.entries(params).map(([key, value]) =>
+    key + '=' + encodeURIComponent(value)
+  ).join('&')
+
+const buildUrl = (base, params) =>
+  base + '?' + buildUrlParams(params)
+
+// Config
+const baseUrl = 'https://twitter.com/intent/tweet'
+const code = 'window.getSelection().toString()'
+
+const separator = ' / '
+const buildText = (title, selection) =>
+  selection ? quote(selection) + separator + title : title
+
+// Entry Point
+chrome.browserAction.onClicked.addListener(({url, title}) =>
+  chrome.tabs.executeScript({ code }, results =>
+    chrome.tabs.create({
+      url: buildUrl(baseUrl, { url, text: buildText(title, first(results)) }
+      )
+    })
+  )
+)
