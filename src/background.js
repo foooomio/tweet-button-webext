ff524f3e5a2c7d36d36f7ab1777014435e9e1e8a
foooomio <foooomio@gmail.com>
2018-02-20 00:54:22 +0900

    Fix tab opening position

--- src/background.js
+++ src/background.js
@@ -27,11 +27,11 @@ const buildText = (title, selection) =>
   selection ? quote(selection) + separator + title : title
 
 // Entry Point
-chrome.browserAction.onClicked.addListener(({url, title}) =>
+chrome.browserAction.onClicked.addListener(({ url, title, index }) =>
   chrome.tabs.executeScript({ code }, results =>
     chrome.tabs.create({
-      url: buildUrl(baseUrl, { url, text: buildText(title, first(results)) }
-      )
+      url: buildUrl(baseUrl, { url, text: buildText(title, first(results)) }),
+      index: index + 1
     })
   )
 )
