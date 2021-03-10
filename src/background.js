111b7f997181805b206f9f4f25caea3eb5e9980b
ypc2e55orj <ypc2e55orj@gmail.com>
2021-03-10 21:41:33 +0900

    Force percent-encoding of fragment

--- src/background.js
+++ src/background.js
@@ -6,6 +6,7 @@ const optionName = isFirefox ? 'allowScriptsToClose' : 'setSelfAsOpener'
 const quote = text => '“' + text + '”'
 const buildText = (title, selection) => quote(selection) + ' / ' + title
 const buildUrl = (base, params) => base + '?' + new URLSearchParams(params)
+const encodeFragment = fragment => '#' + encodeURIComponent(fragment.slice(1)).replace(/[\.!~\*'\(\)]/g, c => '%' + c.charCodeAt(0).toString(16))
 
 const baseUrl = 'https://twitter.com/intent/tweet'
 const code = 'window.getSelection().toString()'
@@ -15,8 +16,9 @@ const height = 420
 chrome.browserAction.onClicked.addListener(({ url, title }) => {
   chrome.tabs.executeScript({ code }, ([ selection ] = []) => {
     const text = selection ? buildText(title, selection) : title
+    const urlEncodedFragment = url.replace(/#.*$/, fragment => encodeFragment(decodeURIComponent(fragment)))
     chrome.windows.create({
-      url: buildUrl(baseUrl, { url, text }),
+      url: buildUrl(baseUrl, { url: urlEncodedFragment, text }),
       left: Math.round((screen.width - width) / 2),
       top: Math.round((screen.height - height) / 2),
       width,
