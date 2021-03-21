5c7872eb394e52dfaf7c6dc50ff68fd369c33b2a
foooomio <foooomio@gmail.com>
2021-03-21 22:30:55 +0900

    Add prettier

--- src/background.js
+++ src/background.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const quote = text => '“' + text + '”'
+const quote = (text) => '“' + text + '”'
 const buildText = (title, selection) => quote(selection) + ' / ' + title
 const buildUrl = (base, params) => base + '?' + new URLSearchParams(params)
 
@@ -10,7 +10,7 @@ const width = 550
 const height = 420
 
 chrome.browserAction.onClicked.addListener(({ url, title }) => {
-  chrome.tabs.executeScript({ code }, ([ selection ] = []) => {
+  chrome.tabs.executeScript({ code }, ([selection] = []) => {
     const text = selection ? buildText(title, selection) : title
     chrome.windows.create({
       url: buildUrl(baseUrl, { url, text }),
